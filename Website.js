47e1d4148a060cbee43e69fb8afc0b8bec5f870d
ranashobi <147505177+ranashobi@users.noreply.github.com>
2023-10-20 03:13:55 +0500

    Update Website.js

--- Website.js
+++ Website.js
@@ -1,66 +1,125 @@
+const itemsPerPage = 15; // Number of items per page
+const ulList = document.querySelector("#box-4 ul");
+const listItems = ulList.getElementsByTagName("li");
+const totalPages = Math.ceil(listItems.length / itemsPerPage);
+let currentPage = 1;
+const maxButtonsToShow = 5;
+let startIndex = 1;
 
-    const itemsPerPage = 14; // Number of items per page
-    const ulList = document.querySelector("#box-4 ul");
-    const listItems = ulList.getElementsByTagName("li");
-    const totalPages = Math.ceil(listItems.length / itemsPerPage);
-    let currentPage = 1;
+// Initialize pagination
+createPaginationButtons();
 
-    // Initialize pagination
-    createPaginationButtons();
+function createPaginationButtons() {
+  const pagination = document.querySelector("#pagination");
+  const prevButton = document.querySelector("#prev");
+  const nextButton = document.querySelector("#next");
 
-    function createPaginationButtons() {
-        const pagination = document.querySelector("#pagination");
-        const prevButton = document.querySelector("#prev");
-        const nextButton = document.querySelector("#next");
+  // Remove existing buttons
+  while (pagination.firstChild) {
+    pagination.removeChild(pagination.firstChild);
+  }
 
-        // Remove existing buttons
-        while (pagination.firstChild) {
-            pagination.removeChild(pagination.firstChild);
-        }
+  // Add the "Prev" button
+  pagination.appendChild(prevButton);
 
-        // Add the "Prev" button
-        pagination.appendChild(prevButton);
-
-        // Add numbered buttons for each page
-        for (let i = 1; i <= totalPages; i++) {
-            const button = document.createElement("button");
-            button.textContent = i;
-            button.onclick = function () {
-                goToPage(i);
-            };
-            if (i === currentPage) {
-                button.classList.add("active");
-            }
-            pagination.appendChild(button);
-        }
-
-        // Add the "Next" button
-        pagination.appendChild(nextButton);
+  // Add numbered buttons for each page
+  for (let i = startIndex; i <= totalPages && i < startIndex + maxButtonsToShow; i++) {
+    const button = document.createElement("button");
+    button.textContent = i;
+    button.onclick = function () {
+      goToPage(i);
+    };
+    if (i === currentPage) {
+      button.classList.add("active");
     }
+    pagination.appendChild(button);
+  }
 
-    function goToPage(page) {
-        currentPage = page;
-        const start = (currentPage - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
+  // Add the "Next" button
+  pagination.appendChild(nextButton);
+}
 
-        for (let i = 0; i < listItems.length; i++) {
-            listItems[i].style.display = (i >= start && i < end) ? "list-item" : "none";
-        }
+function goToPage(page) {
+  currentPage = page;
+  const start = (currentPage - 1) * itemsPerPage;
+  const end = start + itemsPerPage;
 
-        createPaginationButtons();
-    }
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.display = i >= start && i < end ? "list-item" : "none";
+  }
 
-    function prevPage() {
-        if (currentPage > 1) {
-            goToPage(currentPage - 1);
-        }
-    }
+  if (currentPage > startIndex + maxButtonsToShow - 1) {
+    startIndex = currentPage - maxButtonsToShow + 1;
+  } else if (currentPage < startIndex) {
+    startIndex = currentPage;
+  }
 
-    function nextPage() {
-        if (currentPage < totalPages) {
-            goToPage(currentPage + 1);
-        }
-    }
+  createPaginationButtons();
+}
 
-    // Initially show the first page
-    goToPage(1);
+function prevPage() {
+  if (currentPage > 1) {
+    goToPage(currentPage - 1);
+  }
+}
+
+function nextPage() {
+  if (currentPage < totalPages) {
+    goToPage(currentPage + 1);
+  }
+}
+
+// Initially show the first page
+goToPage(1);
+
+
+
+
+
+
+
+
+
+
+
+
+// Search Function Code Start from here
+
+     // Get the necessary elements
+     const searchInput = document.getElementById('searchInput');
+     const searchButton = document.getElementById('searchButton');
+     const itemList = document.getElementById('itemList');
+     
+     // Add items to Box-4
+     // for (let i = 1; i <= 100; i++) {
+     //     const item = `Item ${i}`;
+     //     const li = document.createElement('li');
+     //     li.textContent = item;
+     //     itemList.appendChild(li);
+     // }
+     
+     // Search function
+     function searchItems() {
+         const searchTerm = searchInput.value.toLowerCase();
+         const items = itemList.getElementsByTagName('li');
+     
+         for (const item of items) {
+             const text = item.textContent.toLowerCase();
+             if (text.includes(searchTerm)) {
+                 item.style.display = 'block';
+             } else {
+                 item.style.display = 'none';
+             }
+         }
+     }
+     
+     // Event listeners
+     searchButton.addEventListener('click', searchItems);
+     
+     searchInput.addEventListener('keyup', (event) => {
+         if (event.key === 'Enter') {
+             searchItems();
+         }
+     });
+     
+     searchInput.addEventListener('input', searchItems);
